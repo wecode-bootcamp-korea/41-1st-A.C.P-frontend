f7bc4dc289cb9cf0d6254aab48102559ccf11e7e
Work <seoyunkang@Seoyunui-MacBookPro.local>
2023-01-06 20:46:19 +0900

    Add : Nav Bar 레이아웃 추가

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,10 +5,22 @@ import './NavBar.scss';
 export default function NavBar() {
   return (
     <nav className="navBar">
-      <Link to="/">Logo</Link>
-      <Link to="/products">Products</Link>
-      <Link to="/cart">Cart</Link>
-      <Link to="/login">Login</Link>
+      <ul className="navBarLeft">
+        <li className="menu">
+          <Link to="/products">Plants</Link>
+        </li>
+        <li className="menu">
+          <Link to="/products">Materials</Link>
+        </li>
+        <li className="menu">
+          <Link to="/menulist">Search</Link>
+        </li>
+      </ul>
+      <div className="navBarRight">
+        <Link to="/login">Login</Link>
+        <Link to="/cart">Cart</Link>
+        <Link to="/cart">Archive</Link>
+      </div>
     </nav>
   );
 }
