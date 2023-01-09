27385983344d39ae730c48336bd78eb4aab00e77
Work <seoyunkang@Seoyunui-MacBookPro.local>
2023-01-09 15:17:42 +0900

    Refactor : MenuList 리뷰 반영

--- src/components/NavBar/MenuList.js
+++ src/components/NavBar/MenuList.js
@@ -3,45 +3,6 @@ import { Link } from 'react-router-dom';
 import './MenuList.scss';
 
 export default function MenuList() {
-  const BIG_CATE_MENU = [
-    {
-      id: 1,
-      title: '카테고리',
-      categoryItems: [
-        '식물 모두 보기',
-        '천남성과',
-        '덩굴식물',
-        '양치식물',
-        '소철과 허브',
-        '선인장',
-        '목본류',
-      ],
-    },
-  ];
-
-  const CATE_MENU = [
-    {
-      id: 1,
-      title: '위치',
-      categoryItems: ['Houseplant', 'Hanging', 'Desk', 'Window', 'Outdoor'],
-    },
-    {
-      id: 2,
-      title: '분위기',
-      categoryItems: ['Warm', 'Adorable', 'Cozy', 'Modern'],
-    },
-    {
-      id: 3,
-      title: '크기',
-      categoryItems: ['miniature', '20 - 50cm', '50 - 100cm', '100 - 150cm'],
-    },
-    {
-      id: 4,
-      title: '난이도',
-      categoryItems: ['Easy', 'Normal', 'Hard'],
-    },
-  ];
-
   return (
     <div className="menuList">
       <div className="logoPart">
@@ -50,8 +11,7 @@ export default function MenuList() {
         </Link>
       </div>
       <div className="categoryLeft">
-        {BIG_CATE_MENU.map(bigCategory => {
-          const { id, title, categoryItems } = bigCategory;
+        {BIG_CATE_MENU.map(({ id, title, categoryItems }) => {
           return (
             <ul key={id} className="categoryList">
               <li className="bigCategoryTitle">{title}</li>
@@ -69,8 +29,7 @@ export default function MenuList() {
         })}
       </div>
       <div className="categoryRight">
-        {CATE_MENU.map(category => {
-          const { id, title, categoryItems } = category;
+        {CATE_MENU.map(({ id, title, categoryItems }) => {
           return (
             <ul key={id} className="categoryList">
               <li className="categoryTitle">{title}</li>
@@ -96,3 +55,42 @@ export default function MenuList() {
     </div>
   );
 }
+
+const BIG_CATE_MENU = [
+  {
+    id: 1,
+    title: '카테고리',
+    categoryItems: [
+      '식물 모두 보기',
+      '천남성과',
+      '덩굴식물',
+      '양치식물',
+      '소철과 허브',
+      '선인장',
+      '목본류',
+    ],
+  },
+];
+
+const CATE_MENU = [
+  {
+    id: 1,
+    title: '위치',
+    categoryItems: ['Houseplant', 'Hanging', 'Desk', 'Window', 'Outdoor'],
+  },
+  {
+    id: 2,
+    title: '분위기',
+    categoryItems: ['Warm', 'Adorable', 'Cozy', 'Modern'],
+  },
+  {
+    id: 3,
+    title: '크기',
+    categoryItems: ['miniature', '20 - 50cm', '50 - 100cm', '100 - 150cm'],
+  },
+  {
+    id: 4,
+    title: '난이도',
+    categoryItems: ['Easy', 'Normal', 'Hard'],
+  },
+];
