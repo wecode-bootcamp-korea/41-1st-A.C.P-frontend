a7a55f4b0447ef8447ef7c8b103e5ef204460418
sangheon <sangheon@sangheonui-MacBookAir.local>
2023-01-09 21:04:14 +0900

    Add : 장바구니 레이아웃 구현 완료

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,5 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import OrderRight from '../../components/OrderRight/OrderRight';
+import CartItem from './CartItem/CartItem';
+import './Cart.scss';
+import './CartItem/CartItem.scss';
 
 export default function Cart() {
-  return <div>Cart</div>;
+  const [cartItems, setCartItems] = useState([]);
+  const [cartTotalPrice, setCartTotalPrice] = useState();
+
+  useEffect(() => {
+    // 장바구니 데이터 가져오기
+    fetch('/data/cart.json')
+      .then(res => res.json())
+      .then(data => {
+        setCartItems(data);
+
+        const itemTotalPrice = data.reduce(
+          (acc, curr) => acc + parseInt(curr.price),
+          0
+        );
+
+        setCartTotalPrice(itemTotalPrice);
+      });
+  }, []);
+
+  return (
+    <section className={`cart${cartItems.length > 0 ? '' : ' empty'}`}>
+      <div className="innerCart">
+        <article className="cartLeft">
+          {/* 타이틀 */}
+          <h2 className="titleArticle">장바구니</h2>
+
+          <div className="wrapItems">
+            {/* 전체선택 */}
+            {cartItems.length > 0 ? (
+              <>
+                <div className="checkBox all">
+                  <input type="checkbox" className="inpChk" id="allCheck" />
+                  <label className="label">전체선택</label>
+                </div>
+                <div className="cartList">
+                  {cartItems.map(data => {
+                    return <CartItem key={data.id} data={data} />;
+                  })}
+                </div>
+              </>
+            ) : (
+              <p className="txtCart">상품이 존재하지 않습니다.</p>
+            )}
+          </div>
+        </article>
+        {cartItems.length > 0 && <OrderRight cartTotalPrice={cartTotalPrice} />}
+      </div>
+    </section>
+  );
 }
