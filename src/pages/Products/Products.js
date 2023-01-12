5f9424a41e2874439002dc87ab31b6ee09267b0c
sangheon <sangheon@sangheonui-MacBookAir.local>
2023-01-12 18:41:04 +0900

    Modify : 로그인 성공시 토큰 추가

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -9,6 +9,56 @@ export default function Products() {
   const [plus, setPlus] = useState(1);
   const [modal, setModal] = useState(false);
   const { species, categoryInfo } = FilterData;
+  const [productList, setProductList] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const defaultLimit = 6;
+  let limit = Number(searchParams.get('_limit')) || defaultLimit;
+  const currentCount = limit / defaultLimit;
+  const maxLength = Math.floor(productList.length / defaultLimit) || 1;
+
+  useEffect(() => {
+    navigate('/products');
+  }, []);
+
+  // useEffect(() => {
+  //   fetch(`http://10.58.52.135:3000/lists/filter?${searchParams.toString()}`, {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json;charset=utf-8',
+  //     },
+  //   })
+  //     .then(res => res.json())
+  //     .then(data => setProductList(data));
+  // }, [searchParams]);
+
+  useEffect(() => {
+    fetch('/data/productData.json')
+      .then(res => res.json())
+      .then(data => setProductList(data));
+  }, []);
+
+  const handleMoreClick = () => {
+    if (productList.length > limit) {
+      limit += 6;
+      searchParams.set('_limit', limit);
+      setSearchParams(searchParams);
+
+      // url 변경 필요
+      fetch(
+        `http://10.58.52.135:3000/lists/filter?${searchParams.toString()}`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+          },
+        }
+      )
+        .then(res => res.json())
+        .then(data => setProductList(data));
+    }
+  };
 
   return (
     <>
