caf1449816002f1b121e1803757321d3eadb4dc1
오현주 <hyunjoooh@ohyeonjuui-MacBookPro.local>
2023-01-21 18:21:25 +0900

    몰라

--- src/config.js
+++ src/config.js
@@ -1,7 +1,12 @@
 // src/config.js
-const BASE_URL = 'http://43.201.37.226:3000/';
-export const GET_PLANTS_API = `${BASE_URL}plants`;
-export const GET_USER_INFO_API = `${BASE_URL}users/signIn`;
-export const RESIST_USER_INFO_API = `${BASE_URL}users/signUp`;
+const BASE_URL = 'http://43.201.37.226:3000';
+export const FETCH_SIGN_IN_API = `${BASE_URL}/users/signIn`;
+export const FETCH_SIGN_UP_API = `${BASE_URL}/users/signUp`;
 
-export const GET_CART_API = `${BASE_URL}carts`;
+export const FETCH_PLANTS_API = `${BASE_URL}/plants`;
+export const FETCH_NUTRIENTS_API = `${BASE_URL}/nutrients`;
+export const FETCH_POTS_API = `${BASE_URL}/pots`;
+
+export const FETCH_CART_API = `${BASE_URL}/carts`;
+export const FETCH_ORDER_API = `${BASE_URL}/orders`;
+export const FETCH_ORDERED_API = `${BASE_URL}/ordered`;
